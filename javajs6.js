01b1b4b2065075f3e8b14b83c9c21cba83270d01
Student <student@RU-002.local>
2016-01-22 14:16:15 -0700

    need help pushing

--- javajs6.js
+++ javajs6.js
@@ -3,14 +3,14 @@ angular.module('moduleOne',[])
 angular.module('moduleOne')
 	.controller('controllerOne', ['$scope', function($scope){
 		$scope.hide = true
-		$scope.data = ['Tom THE GRINDER Lovett', '50 Shades of Coding and anythin by Nicholas Sparks',
+		$scope.data = ['Tom "THE GRINDER" Lovett', '50 Shades of Coding and anything by Nicholas Sparks',
 		'I love the angular library but could not find the front door', 
 		'I spent 3 years in a Village People Cover band, and then as a "lotion holder" for parapalegic masseuse. \
 		Tom refers to himself in the third person when cute girls are near by because Tom is the coolest guy \
 		 Tom knows.']
 		
-
-
+		 $scope.users = []
+		 $scope.profile = {name:'', books:'', library:'', bio:''}
 		$scope.formOpen = function() {
 			$scope.show = true
 			$scope.hide = false
@@ -20,10 +20,15 @@ angular.module('moduleOne')
 			$scope.hide = true
 		}
 		$scope.submit = function() {
-			$scope.data[0] = $scope.name
-			$scope.data[1] = $scope.books
-			$scope.data[2] = $scope.library
-			$scope.data[3] = $scope.bio
+			console.log($scope.users)
+			
+			$scope.users.push($scope.profile)
+			
+			console.log($scope.profile)
+			$scope.data[0] = $scope.users[0].name
+			$scope.data[1] = $scope.users[0].books	
+			$scope.data[2] = $scope.users[0].library
+			$scope.data[3] = $scope.users[0].bio
 
 		}
 
